821bc3f122ed705a8abc04d2b285ded0babc74db
Pedro Zaroni <zaronipedro@outlook.com>
2017-06-23 01:24:49 -0300

    Update GUI

--- runner.py
+++ runner.py
@@ -3,11 +3,13 @@ import sys
 from PyQt4 import QtGui, QtCore
 
 import design as ui
+import button_listener as bt
 
 class Application(QtGui.QMainWindow, ui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(Application, self).__init__(parent)
         self.setupUi(self)
+        bt.button_listener(self)
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
