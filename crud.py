721286ae45f7bc8cb9fef736933ec62c13005280
Matheus de Oliveira Campos <matheuscampos@gec.inatel.br>
2017-06-22 21:47:20 -0300

    crud e model

--- crud.py
+++ crud.py
@@ -5,28 +5,28 @@ from connector import DataBaseConnector
 
 def connect():
     conn = DataBaseConnector()
-    conn.get_db()
+    db = conn.get_db()
     if conn != 1:
-        return conn
-    return conn
+        return db
+    return db
 
 class AlunoDAO(object):
 
     @staticmethod
     def insert(aluno):
         db = connect()
+        print db
 
         if db != 1:
             cur = db.cursor()
             try:
-                cur.execute("INSERT INTO aluno (Nome, Matricula, Curso, SemestreAtual, Idade, Curso_idCurso)"
-                            "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}')"
-                            .format(aluno.nome, aluno.matricula, aluno.curso, aluno.semestre, aluno.idade, aluno.curso))
+                cur.execute("INSERT INTO aluno (Nome, Matricula, SemestreAtual, Idade, Curso_idCurso)"
+                            "VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')"
+                            .format(aluno.nome, aluno.matricula, aluno.semestre, aluno.idade, aluno.curso))
                 db.commit()
             except MySQLdb.Error:
                 print "Error to execute script"
             finally:
-                print "Concluido"
                 cur.close()
 
 
